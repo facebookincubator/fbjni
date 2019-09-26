c570257ca245157efc0273122d122954c2159361
Pascal Hartig <phartig@rdrei.net>
2019-09-26 15:54:33 +0100

    switch to android cmake

--- scripts/run-host-tests.sh
+++ scripts/run-host-tests.sh
@@ -2,13 +2,14 @@
 
 set -exo pipefail
 
-mkdir -p host-build-cmake
+CMAKE=$ANDROID_HOME/cmake/3.10.2.4988404/bin/cmake
+
 # Configure CMake project
-cmake -S . -B host-build-cmake -DJAVA_HOME="$JAVA_HOME"
+$CMAKE -S . -B host-build-cmake -DJAVA_HOME="$JAVA_HOME"
 # Build binaries and libraries
-cmake --build host-build-cmake -j 8
+$CMAKE --build host-build-cmake -j 8
 # Run C++ tests
-cmake --build host-build-cmake --target test
+$CMAKE --build host-build-cmake --target test
 # LD_LIBRARY_PATH is needed for native library dependencies to load cleanly
 TEST_LD_LIBRARY_PATH=$PWD/host-build-cmake:$PWD/host-build-cmake/test/jni
 # Build and run JNI tests
