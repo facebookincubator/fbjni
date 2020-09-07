73ecdae3a4f5e1fd82bf627b4060f18fb21d57c7
Elijah Rippeth <elijah.rippeth@gmail.com>
2020-09-07 04:54:56 -0400

    make tests more straightforward for debugging.

--- scripts/run-host-tests.sh
+++ scripts/run-host-tests.sh
@@ -15,21 +15,25 @@
 
 set -exo pipefail
 
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
-CMAKE=$ANDROID_HOME/cmake/3.10.2.4988404/bin/cmake
+CMAKE="$ANDROID_HOME/cmake/3.10.2.4988404/bin/cmake"
 export CXX=clang++
 
-mkdir -p "$BASE_DIR/host-build-cmake"
-cd "$BASE_DIR/host-build-cmake"
+mkdir -p "host-build-cmake"
+cd "host-build-cmake"
 
 # Configure CMake project
 $CMAKE -DJAVA_HOME="$JAVA_HOME" ..
+
 # Build binaries and libraries
-make
+$CMAKE --build .
+
 # Run C++ tests
 make test
+
+cd ..
+
 # LD_LIBRARY_PATH is needed for native library dependencies to load cleanly
-TEST_LD_LIBRARY_PATH="$BASE_DIR/host-build-cmake:$BASE_DIR/host-build-cmake/test/jni"
+export LD_LIBRARY_PATH="./host-build-cmake:./host-build-cmake/test/jni"
+
 # Build and run JNI tests
-cd "$BASE_DIR"
-env LD_LIBRARY_PATH="$TEST_LD_LIBRARY_PATH" ./gradlew -b host.gradle -PbuildDir=host-build-gradle test
+./gradlew -b host.gradle -PbuildDir=host-build-gradle test
