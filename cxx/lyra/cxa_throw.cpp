231bed1f746e665abe614671284067cab6fb011a
Shoaib Meenai <smeenai@meta.com>
2024-06-17 14:35:10 -0700

    Remove libstdc++ support
    Summary:
    Android only uses libc++ now. The libc++ implementation should also work
    for libstdc++ though (for other platforms if desired). The libc++
    implementation doesn't rely on any headers not available on Windows so
    we can remove the `_WIN32` conditionals (though it still won't work on
    Windows because of ABI library differences, but that's not different
    from before).
    
    Reviewed By: thevinster
    
    Differential Revision: D58622935
    
    fbshipit-source-id: aa55809d570101c1be97184feb8255aa61bb66eb

--- cxx/lyra/cxa_throw.cpp
+++ cxx/lyra/cxa_throw.cpp
@@ -19,10 +19,6 @@
 #include <mutex>
 #include <stdexcept>
 #include <unordered_map>
-#ifndef _WIN32
-#include <cxxabi.h>
-#include <unwind.h>
-#endif
 
 #include <lyra/lyra_exceptions.h>
 
@@ -52,8 +48,6 @@ void enableCxaThrowHookBacktraces(bool enable) {
 
 [[gnu::noreturn]] __attribute__((annotate("dynamic_fn_ptr"))) void (
     *original_cxa_throw)(void*, const std::type_info*, void (*)(void*));
-
-#if defined(_LIBCPP_VERSION)
 
 // We want to attach stack traces to C++ exceptions. Our API contract is that
 // calling lyra::getExceptionTrace on the exception_ptr for an exception should
@@ -162,95 +156,6 @@ const ExceptionTraceHolder* detail::getExceptionTraceHolder(
   // the exception (to support e.g. fbthrow).
   return getExceptionTraceHolderInException(ptr);
 }
-#elif !defined(_WIN32)
-
-namespace {
-
-const auto traceHolderType =
-    static_cast<const abi::__class_type_info*>(&typeid(ExceptionTraceHolder));
-
-// lyra's __cxa_throw attaches stack trace information to thrown exceptions. It
-// basically does:
-//   1. capture stack trace
-//   2. construct a new type_info struct that:
-//     a. holds the ExceptionTraceHolder
-//     b. supports upcasting to lyra::ExceptionTraceHolder* (by just returning
-//     the holder member) c. acts like the original exception type_info
-//     otherwise
-//   3. call original __cxa_throw() with original exception pointer, the
-//      HijackedExceptionTypeInfo, and HijackedExceptionTypeInfo::destructor
-//      (which will both delete the constructed type info and call the original
-//      destructor).
-struct HijackedExceptionTypeInfo : public abi::__class_type_info {
-  HijackedExceptionTypeInfo(
-      void* obj,
-      const std::type_info* base,
-      void (*destructor)(void*))
-      : abi::__class_type_info{base->name()},
-        base_{base},
-        orig_dest_{destructor} {}
-
-  bool __is_pointer_p() const override {
-    return base_->__is_pointer_p();
-  }
-
-  bool __is_function_p() const override {
-    return base_->__is_function_p();
-  }
-
-  bool __do_catch(
-      const type_info* __thr_type,
-      void** __thr_obj,
-      unsigned __outer) const override {
-    return base_->__do_catch(__thr_type, __thr_obj, __outer);
-  }
-
-  bool __do_upcast(const abi::__class_type_info* __target, void** __obj_ptr)
-      const override {
-    if (__target == traceHolderType) {
-      *__obj_ptr = (void*)&stack_;
-      return true;
-    }
-    return base_->__do_upcast(__target, __obj_ptr);
-  }
-
-  static void destructor(void* obj) {
-    auto exc_ptr = reinterpret_cast<std::exception_ptr*>(&obj);
-    auto info = reinterpret_cast<const ::std::type_info*>(
-        exc_ptr->__cxa_exception_type());
-    auto mutable_info = static_cast<HijackedExceptionTypeInfo*>(
-        const_cast<std::type_info*>(info));
-    if (mutable_info->orig_dest_) {
-      mutable_info->orig_dest_(obj);
-    }
-    delete mutable_info;
-  }
-
- private:
-  const std::type_info* base_;
-  void (*orig_dest_)(void*);
-  ExceptionTraceHolder stack_;
-};
-
-} // namespace
-
-[[noreturn]] void
-cxa_throw(void* obj, const std::type_info* type, void (*destructor)(void*)) {
-  if (enableBacktraces.load(std::memory_order_relaxed)) {
-    if (!type->__do_upcast(traceHolderType, &obj)) {
-      type = new HijackedExceptionTypeInfo(obj, type, destructor);
-      destructor = HijackedExceptionTypeInfo::destructor;
-    }
-  }
-  original_cxa_throw(obj, type, destructor);
-}
-
-const ExceptionTraceHolder* detail::getExceptionTraceHolder(
-    std::exception_ptr ptr) {
-  return getExceptionTraceHolderInException(ptr);
-}
-
-#endif // libc++/Windows
 
 } // namespace lyra
 } // namespace facebook
